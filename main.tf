e40b4bdda17843823bef587922619bab2b645ff3
pcs1999.chandra shekar pendem <chandupcs1999@gmail.com>
2023-09-04 10:11:45 +0530

    terraform_vpc_infra

--- main.tf
+++ main.tf
@@ -1,3 +1,10 @@
-module "vpc" {
+
+
+module "network_vpc" {
   source = "github.com/chandra5141/tf-module-vpc.git"
+  env= var.env
+
+  for_each   = var.vpc
+  cidr_block = each.value.cidr_block
+  subnets_cidr = var.subnets_cidr
 }

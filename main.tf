4a6bf1163baf9fb7af24c15189ebee0d5ec2be32
pcs1999.chandra shekar pendem <chandupcs1999@gmail.com>
2023-09-06 12:07:04 +0530

    terraform_vpc_infra

--- main.tf
+++ main.tf
@@ -19,9 +19,7 @@ module "subnets" {
   igw                       = module.network_vpc.igw
   cidr_block                = each.value.cidr_block
   availability_zone         = each.value.availablity_zone
-  default_vpc_id            = var.default_vpc_id
   igw_id                    = module.network_vpc.igw_id
-
   name                      = each.value.name
   vpc_id                    = lookup(lookup(module.network_vpc,each.value.vpc_name.null),"vpc_id",null)
   vpc_peering_connection_id = lookup(lookup(module.network_vpc,each.value.vpc_name.null),"vpc_peering_connection_id",null)

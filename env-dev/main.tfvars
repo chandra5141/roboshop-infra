dff23576c4a9ff9ae21ae9db797d0058399a38ad
pcs1999.chandra shekar pendem <chandupcs1999@gmail.com>
2023-09-06 11:46:05 +0530

    terraform_vpc_infra

--- env-dev/main.tfvars
+++ env-dev/main.tfvars
@@ -4,7 +4,39 @@ default_vpc_id = "vpc-024f86141cbbc02e6"
 vpc = {
   main = {
     cidr_block = "10.0.0.0/16"
-    public_subnets_cidr = ["10.0.0.0/24", "10.0.1.0/24"]
-    private_subnets_cidr = ["10.0.2.0/24", "10.0.3.0/24"]
+    availablity_zones = ["us-east-1a", "us-east-1b"]
+  }
+}
+
+subnets ={
+  public = {
+    name = "public"
+    vpc_name= "main"
+    cidr_block = ["10.0.0.0/24", "10.0.1.0/24"]
+    availability_zone = ["us-east-1a", "us-east-1b"]
+  }
+
+  web = {
+    name = "web"
+    vpc_name= "main"
+    cidr_block = ["10.0.2.0/24", "10.0.3.0/24"]
+    availability_zone = ["us-east-1a", "us-east-1b"]
+
+  }
+
+  app = {
+    name = "app"
+    vpc_name= "main"
+    cidr_block = ["10.0.4.0/24", "10.0.5.0/24"]
+    availability_zone = ["us-east-1a", "us-east-1b"]
+
+  }
+
+  db = {
+    name = "db"
+    vpc_name= "main"
+    cidr_block = ["10.0.6.0/24", "10.0.7.0/24"]
+    availability_zone = ["us-east-1a", "us-east-1b"]
+
   }
 }
--- main.tf
+++ main.tf
@@ -1,12 +1,30 @@
 
 module "network_vpc" {
-  source = "github.com/chandra5141/tf-module-vpc.git"
-  env= var.env
+  source          = "github.com/chandra5141/tf-module-vpc.git"
+  env             = var.env
 
-  for_each   = var.vpc
-  cidr_block = each.value.cidr_block
-  public_subnets_cidr = each.value.public_subnets_cidr
-  private_subnets_cidr = each.value.private_subnets_cidr
+  for_each        = var.vpc
+  cidr_block      = each.value.cidr_block
+  default_vpc_id  = var.default_vpc_id
+  public_subnets_ids     = lookup(module.subnets, "public" , null), "subnets_ids" , null)
+}
 
-  default_vpc_id = var.default_vpc_id
+module "subnets" {
+  source                    = "github.com/chandra5141/tf-module-subnets.git"
+  env                       = var.env
+  default_vpc_id            = var.default_vpc_id
+
+
+  for_each                  = var.subnets
+  igw                       = module.network_vpc.igw
+  cidr_block                = each.value.cidr_block
+  availability_zone         = each.value.availablity_zone
+  default_vpc_id            = var.default_vpc_id
+  igw_id                    = module.network_vpc.igw_id
+
+  name                      = each.value.name
+  vpc_id                    = lookup(lookup(module.network_vpc,each.value.vpc_name.null),"vpc_id",null)
+  vpc_peering_connection_id = lookup(lookup(module.network_vpc,each.value.vpc_name.null),"vpc_peering_connection_id",null)
+  internet_gateway_id       = lookup(lookup(module.network_vpc,each.value.vpc_name.null),"internet_gateway_id",null)
+
 }

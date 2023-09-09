b0c8fd1e45e65b648db2868c03a75ef06a5aea45
pcs1999.chandra shekar pendem <chandupcs1999@gmail.com>
2023-09-09 19:23:20 +0530

    terraform_vpc&docdb_infra

--- main.tf
+++ main.tf
@@ -9,7 +9,7 @@ module "network_vpc" {
   cidr_block             = each.value.cidr_block
   public_subnets         = each.value.public_subnets
   private_subnets        = each.value.private_subnets
-  availability_zone      = each.value.availablity_zone
+  availability_zone      = each.value.availability_zone
 
 }
 
